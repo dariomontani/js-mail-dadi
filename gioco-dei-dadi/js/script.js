821d3da33b9f0c188e2c7bbe015cd3b3dd11668d
dariomontani <dariomontani@gmail.com>
2021-11-24 17:14:32 +0100

    numero random per user e computer

--- gioco-dei-dadi/js/script.js
+++ gioco-dei-dadi/js/script.js
@@ -2,3 +2,9 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+// Genero un numero random da 1 a 6, sia per il giocatore sia per il computer.
+const numberRandomUser = Math.floor(Math.random() * 6) + 1;
+const numberRandomComputer = Math.floor(Math.random() * 6) + 1;
+
+console.log(numberRandomUser);
+console.log(numberRandomComputer);

d58b749a5fbffe8414ab1cb0586066e958f4c277
dariomontani <dariomontani@gmail.com>
2021-11-24 15:24:38 +0100

    lista email

---
+++ mail/js/script.js
@@ -0,0 +1,18 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+// Chiedo all’utente la sua email
+const email = prompt('inserisci qui la tua email')
+
+// controllo che sia nella lista di chi può accedere
+const lista = ["dario@gmail.com", "montani@hotmail.it", "pippo@boolean.it", "pluto35@boolean.com"]
+
+
+
+
+
+
+
+

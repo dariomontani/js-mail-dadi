50a778df8bdfdbda4be3300ef2f88576d7afd69a
dariomontani <dariomontani@gmail.com>
2021-11-24 17:26:00 +0100

    numeri estratti

--- gioco-dei-dadi/js/script.js
+++ gioco-dei-dadi/js/script.js
@@ -2,9 +2,21 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+// gli dico all'utente di inserire il suo nome
+const nameUser = prompt('Inserisci qui il tuo nome per giocare');
+
 // Genero un numero random da 1 a 6, sia per il giocatore sia per il computer.
 const numberRandomUser = Math.floor(Math.random() * 6) + 1;
 const numberRandomComputer = Math.floor(Math.random() * 6) + 1;
 
-console.log(numberRandomUser);
-console.log(numberRandomComputer);
+// verifico chi tra l'utente e il computer ha il numero più alto
+if (numberRandomUser > numberRandomComputer) {
+    console.log(nameUser, 'Hai vinto con il numero', numberRandomUser);
+} else if (numberRandomUser < numberRandomComputer) {
+    console.log(nameUser, 'Hai perso il numero del computer', numberRandomUser, 'è più alto del tuo');
+} else {
+    console.log(nameUser, 'La partita si è conclusa in parità');
+}
+// mostro all'utente i numeri estratti
+console.log('Il Tuo Numero', numberRandomUser);
+console.log('Il numero del computer', numberRandomComputer);
